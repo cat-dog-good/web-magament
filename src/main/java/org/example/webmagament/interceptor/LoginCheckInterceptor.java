ea0f3927ced25a7ef9cf467419769b599a85fb89
qll-NO.1 <zyj647@126.com>
2025-07-13 18:50:59 +0800

    Interceptor快速入门

---
+++ src/main/java/org/example/webmagament/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,27 @@
+package org.example.webmagament.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override  //目标资源方法运行前运行，返回tree：放行，返回false：不放行
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandle...");
+        return true;
+    }
+
+    @Override  //目标资源方法运行后运行
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle...");
+    }
+
+    @Override  //视图渲染完毕后运行，最后运行
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion...");
+    }
+
+}
