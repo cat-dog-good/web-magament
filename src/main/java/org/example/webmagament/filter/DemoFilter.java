3cd49c319bb5f05da04262bedf1c28af08174762
qll-NO.1 <zyj647@126.com>
2025-07-13 13:41:08 +0800

    Filter快速入门:优化

--- src/main/java/org/example/webmagament/filter/DemoFilter.java
+++ src/main/java/org/example/webmagament/filter/DemoFilter.java
@@ -9,18 +9,19 @@ import java.io.IOException;
 public class DemoFilter implements Filter {
     @Override  //初始化方法，只调用一次
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("11");
+        System.out.println("执行初始化方法");
     }
 
     @Override  //拦截到请求后调用，可调用多次
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("ww");
+        System.out.println("放行前逻辑");
         //放行
         filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("放行后逻辑");
     }
 
     @Override  //销毁方法，只调用一次
     public void destroy() {
-        System.out.println("ee");
+        System.out.println("执行销毁方法");
     }
 }
