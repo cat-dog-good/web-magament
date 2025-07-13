d1bd6185cede93c96bccbe656eb23713e294e9cb
qll-NO.1 <zyj647@126.com>
2025-07-13 13:54:39 +0800

    过滤器链快速入门

---
+++ src/main/java/org/example/webmagament/filter/AbcFilter.java
@@ -0,0 +1,19 @@
+package org.example.webmagament.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class AbcFilter implements Filter {
+
+    @Override  //拦截到请求后调用，可调用多次
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Abc放行前逻辑");
+        //放行
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("Abc放行后逻辑");
+    }
+}
+
